ac0df20f950d8d457f6536441255ee9b90ac4b3b
pyd6802 <55672489+pyd6802@users.noreply.github.com>
2021-10-02 20:02:28 -0400

    Amending Javadoc comment

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -4,7 +4,7 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/* Adding a javadoc comment */
+/** Adding a javadoc comment **/
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
