2c18445c22d90fc2c74ca52cc7da273e096b1aa1
pyd6802 <55672489+pyd6802@users.noreply.github.com>
2021-10-07 18:21:44 -0400

    Delete Chapter04 directory

--- Chapter04/exercise1/test_calculator.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from calculator import multiply
-
-class TestSomething(unittest.TestCase):
-	def test_multiply(self):
-		self.assertEqual(6, multiply(2,3))
-
-if __name__ == '__main__':
-	unittest.main()
--- Chapter04/sample2/src/main/java/com/leszko/calculator/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.leszko.calculator;
-import org.springframework.stereotype.Service;
-
-@Service
-public class Calculator {
-     int sum(int a, int b) {
-          return a + b;
-     }
-}
