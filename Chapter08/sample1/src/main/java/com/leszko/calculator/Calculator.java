0447c80801c59fe099235549ef1b49af1cb3949f
pyd6802 <paul@pdwyer.com>
2021-10-16 22:22:54 -0400

    Test checkstyle

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -4,7 +4,7 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-
+/** Javadoc **/
 @Service
 public class Calculator {
         final static int UMLNUMBER1 = 3;
